0d6b0532562def2d7a975108097afa623daecd52
stephen-osullivan <31805109+stephen-osullivan@users.noreply.github.com>
2024-04-24 11:00:59 +0000

    added zipping fuctionality

--- utilities.py
+++ utilities.py
@@ -1,8 +1,10 @@
 import requests
 
 import concurrent.futures
+import os
 import time
 from typing import List, Optional
+import zipfile
 
 def download_image(url: str, save_file: Optional[str] = None) -> bool:
   try:
@@ -15,13 +17,13 @@ def download_image(url: str, save_file: Optional[str] = None) -> bool:
         with open(save_file, "wb") as file:
             file.write(response.content)
             print(save_file, "downloaded successfully!")
-        return True
+        return url, save_file, True
     else:
         print(f"Failed to download {save_file}.")
-        return False
+        return url, save_file, False
   except requests.exceptions.RequestException as e:
         print(f"Error downloading {url}: {e}")
-        return False
+        return url, save_file, False
      
 
 def download_images(
@@ -35,7 +37,8 @@ def download_images(
     if save_files is None:
        save_files = [url.split('/')[-1] for url in urls]
     if save_folder:
-       save_files = [f'{save_folder}/{save_file}' for save_file in save_files]
+        os.makedirs(save_folder, exist_ok=True)
+        save_files = [f'{save_folder}/{save_file}' for save_file in save_files]
     
     with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
         # Submit the tasks to the executor
@@ -43,8 +46,27 @@ def download_images(
         for url, save_file in zip(urls, save_files):
             threads.append(executor.submit(download_image, url, save_file))
         results = []
-        for t in concurrent.futures.as_completed(threads):
-           results.append(t.result())
+        for f in concurrent.futures.as_completed(threads):
+           results.append(f.result())
         return results
         
     print('Total Execution time:', time.time()-t0)
+
+
+def zip_files(zip_filename, file_paths):
+    """
+    Creates a ZIP archive containing the files specified by the file_paths.
+
+    Args:
+        zip_filename (str): The name of the ZIP file to create.
+        file_paths (list): A list of file paths to include in the ZIP archive.
+    
+    Example usage:
+        files_to_zip = ['file1.txt', 'file2.py', 'folder/file3.jpg']
+        zip_files('archive.zip', files_to_zip)
+    """
+    with zipfile.ZipFile(zip_filename, 'w') as zip_file:
+        for file_path in file_paths:
+            zip_file.write(file_path)
+
+
